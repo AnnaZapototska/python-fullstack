e119c0ce3e1b27472669558e453397df66ac623e
AnnaZapototska <zapototskaann@gmail.com>
2023-03-25 18:26:18 +0300

    add

---
+++ WEEK12/DAY4/daychellenge/workwithactors.sql
@@ -0,0 +1,7 @@
+-- SELECT COUNT(*) FROM actors;
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES ('John', 'Doe', NULL, 1); 
+--  null value in column "age" of relation "actors" violates not-null constraint
+
+
+
